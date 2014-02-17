46274ed29da896eac0961268691e4f908f9d9704
luis <luisjdabol@gmail.com>
2014-02-17 18:37:00 -0500

    scaffolding users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Taskmanager::Application.routes.draw do
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Taskmanager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'Taskmanager#index'
 
   # See how all your routes lay out with "rake routes"
 
